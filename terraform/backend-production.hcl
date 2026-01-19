367cf2add2566735c2de18843ace70cbc4e952c1
wachandr <wachandr@cisco.com>
2026-01-19 21:48:36 +0530

    trying to fix state saving in bucket

---
+++ terraform/backend-production.hcl
@@ -0,0 +1,5 @@
+bucket         = "tf-state-prod-998877665544"
+key            = "billing-alarm/prod/terraform.tfstate"
+region         = "us-east-1"
+dynamodb_table = "terraform-locks"
+encrypt        = true
--- terraform/backend-production.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-prod-998877665544"
-    key            = "billing-alarm/prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
